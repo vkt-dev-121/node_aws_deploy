53f9cdf955f6cd6bdc114ffe7768bb3871e31033
Vikas-tiwari-developer <77283008+Vikas-tiwari-developer@users.noreply.github.com>
2023-12-17 11:57:11 +0530

    new api created

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/api/get', (req,res) => {
     res.send({message : 'Nodejs AWS Deployment'});
 })
 
+app.get('/api/get/user', (req,res) => {
+    res.send({name : 'Dev', email: 'dev@gmail.com', 'role': 'Developer'});
+})
+
 app.listen(9000, (req,res) => {
     console.log('server running on port 9000')
 })
